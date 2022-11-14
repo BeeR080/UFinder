f4638bf391d9f42377e9932a9db301cd73f6f571
beer <kipa080@yahoo.com>
2022-11-14 12:36:25 +0300

    Добавил возможность искать по имени и открывать нужный файл. Работаем с интерфейсом

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -7,18 +10,38 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlin.io.path.Path
+
 
 @Preview
 @Composable
 fun App() {
-    var text by remember { mutableStateOf("Ufinder on Kotlin") }
-Button( {
-}){
-    Text(text)
-}
-    var searchUsers = remember { SearchUser("Якушкин Константин.txt") }
+    val searchUsers = remember { SearchUser() }
+    var text by remember { mutableStateOf("Поиск") }
+
+// Интерфейс
+    Row (
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize()
+
+    )
+    {
+        Button(modifier = Modifier
+            ,
+            onClick = {
+                searchUsers.main(Path(SearchUser.URL) , "Шишлаков")
+            })
+        {
+            Text(text)
+
+        }
+    }
+
 }
 
 fun main() = application {
--- src/main/kotlin/SearchUser.kt
+++ src/main/kotlin/SearchUser.kt
@@ -1,24 +1,47 @@
 import java.io.File
 import java.io.FileReader
-import java.nio.file.Files
+import java.lang.Exception
 import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.Path
 
-class SearchUser(var userName:String) {
-    val path = Path(URL)
+class SearchUser() {
 
-        var filePath = File(path.toString(),userName)
-        val openfile = FileReader(filePath)
-        val end = openfile.readLines().takeLast(6)
-        val e = println(end)
+fun main(path: Path,userName:String){
+    findInPath(path,userName)
 
+}
 
 companion object{
     val URL = Paths.get("R:\\IT\\Admin\\Internal\\LogOnOff\\LogOnOff_New")
         .toAbsolutePath()
         .toString()
 }
+
+
 }
+
+fun findInPath(path: Path, userName:String){
+    val f = File(path.toUri())
+    val filelist = f.listFiles()
+    for (files in filelist) {
+        if (files.name.startsWith(userName)) {
+            val filePath = File(path.toString(), files.name)
+            try{
+                val openfile = FileReader(filePath)
+                val endfile = openfile.readLines().takeLast(6)
+                println(endfile)
+                println(files.name)
+            }catch (e: Exception){
+                println(e.toString())
+            }
+        }
+    }
+
+}
+
+
+
+
 
 
